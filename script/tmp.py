078270a027b8a57834cee87d26347fe4528b6f1e
Arimyun <arim.yoon@mpimet.mpg.de>
2022-10-14 14:24:51 +0200

    do something

--- script/tmp.py
+++ script/tmp.py
@@ -1,13 +1,10 @@
-import numpy as np
-import pandas pd
+
+# %%
 import sys
-sys.path.append('/work/mh0731/m300876/package')
-import interp3d
+sys.path.insert(0,'/work/mh0731/m300876/package')
+#%%
 import icons
-from pathlib import Path
-import importlib
-import numpy as np
-import xarray as xr
-from distributed import Client, progress, wait # Libaray to orchestrate distributed resources\
+# %%
 import dask # Distributed data libary
 icons.prepare_cpu(memory='128GB',nworker=1)
+# %%
