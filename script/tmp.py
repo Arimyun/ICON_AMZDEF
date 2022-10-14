6e8c252ea1bc88fb8078137542f2a1899fd546c9
Arimyun <arim.yoon@mpimet.mpg.de>
2022-10-14 15:10:00 +0200

    env finally worked

--- script/tmp.py
+++ script/tmp.py
@@ -1,10 +1,4 @@
-
-# %%
-import sys
-sys.path.insert(0,'/work/mh0731/m300876/package')
 #%%
-import icons
-# %%
-import dask # Distributed data libary
-icons.prepare_cpu(memory='128GB',nworker=1)
+import numpy as np
+import dask
 # %%
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mypackage',
     version='0.0.1',
-    packages=find_packages(),
+    packages=['package'],
     install_requires=[
         'pytest',
         'sphinx',
